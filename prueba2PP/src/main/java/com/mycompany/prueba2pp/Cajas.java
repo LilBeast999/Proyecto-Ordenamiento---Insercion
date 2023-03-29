b942bf49499fdf6067316ba1caf3976ed2cec015
Gustavo T <gustatorres132@gmail.com>
2023-03-29 19:03:04 -0300

    se arregla commit mal ejecutado llamado "Se agregaron mas clases y metodos"

---
+++ prueba2PP/src/main/java/com/mycompany/prueba2pp/Cajas.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba2pp;
+
+/**
+ *
+ * @author gusta
+ */
+public class Cajas {
+    int peso;
+    int posicionx,posiciony;
+        
+    public Cajas() {
+    }
+    
+    public int getPeso(){
+        return this.peso;
+    }
+    
+    public int getPosicionx(){
+        return this.posicionx;
+    }
+    
+    public int getPosiciony(){
+        return this.posiciony;
+    }
+}
---
+++ prueba2PP/src/main/java/com/mycompany/prueba2pp/Lapiz.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba2pp;
+
+/**
+ *
+ * @author gusta
+ */
+public class Lapiz {
+    int posicionx, posiciony;
+
+    public Lapiz(int posicionx, int posiciony) {
+        this.posicionx = posicionx;
+        this.posiciony = posiciony;
+    }
+    
+    public void dibujarfondo(){
+    
+    }
+    
+    public void dibujarcaja(){
+    
+    }
+    
+     public void dibujargrua(){
+    
+    }
+     
+    public void dibujar1(){
+    
+    } 
+    
+    public void dibujar2(){
+    
+    }
+    
+    public void dibujar3(){
+    
+    }
+    
+    public void dibujar4(){
+    
+    }
+    
+    public void dibujar5(){
+    
+    }
+    
+    public void dibujar6(){
+    
+    }
+    
+    public void dibujar7(){
+    
+    }
+    
+    public void dibujar8(){
+    
+    }
+    
+    public void dibujar9(){
+    
+    }
+    
+    public void dibujargancho(){
+    
+    }
+}
