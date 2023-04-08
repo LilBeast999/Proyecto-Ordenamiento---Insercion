c1d40f58e6765c468fdf04241f26597d4c465fe6
Thomas Lizana <tlizana2002@gmail.com>
2023-04-08 19:00:40 -0400

    Cambios del pseudocodigo de esta semana

--- prueba2PP/src/main/java/com/mycompany/prueba2pp/App.java
+++ prueba2PP/src/main/java/com/mycompany/prueba2pp/App.java
@@ -1,6 +1,7 @@
 package com.mycompany.prueba2pp;
 
 import java.util.ArrayList;
+import java.util.Random;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.scene.control.Label;
@@ -11,6 +12,8 @@ import javafx.scene.shape.*;
 import javafx.animation.TranslateTransition;
 import javafx.util.Duration;
 import javafx.animation.SequentialTransition;
+import static javafx.application.Application.launch;
+import javafx.concurrent.Task;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.*;
 import javafx.scene.paint.Color;
@@ -53,7 +56,7 @@ private static final int TIEMPO_ESPERA = 1000; // 1 segundo
         anchor=lapiz.dibujar7();
         anchor=lapiz.dibujar8();
         anchor=lapiz.dibujar9();
-        
+        anchor=Pseudocodigo(anchor);
        
         Label etiqueta = new Label(arreglo.toString());
         anchor.getChildren().add(etiqueta);
@@ -173,16 +176,105 @@ private static final int TIEMPO_ESPERA = 1000; // 1 segundo
     
     }
     public AnchorPane Pseudocodigo(AnchorPane Anchor) {
-    
-    
-    
-    
-    return Anchor; 
+        int[] arreglo =generarArregloAleatorio(9,1,100);
+        
+        Text[] etiquetasCodigo = {
+                new Text("1. Para i = 1 hasta n-1 hacer:"),
+                new Text("2.     j = i"),
+                new Text("3.     mientras j > 0 y A[j-1] > A[j] hacer:"),
+                new Text("4.         intercambiar A[j] y A[j-1]"),
+                new Text("5.         j = j - 1")
+        };
+     
+        Text etiquetaArreglo = new Text(arrayToString(arreglo));
+        VBox root = new VBox(10);
+        root.getChildren().addAll(etiquetasCodigo);
+        root.getChildren().add(etiquetaArreglo);
+        
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                for (int i = 1; i < arreglo.length; i++) {
+                    resaltarLineaCodigo(etiquetasCodigo, 0);
+                    Thread.sleep(TIEMPO_ESPERA);
+                    
+                    int valorActual = arreglo[i];
+                    int j = i;
+                    
+                    resaltarLineaCodigo(etiquetasCodigo, 1);
+                    Thread.sleep(TIEMPO_ESPERA);
+                    
+                    while (j > 0 && arreglo[j - 1] > valorActual) {
+                        arreglo[j] = arreglo[j - 1];
+                        j--;
+                        
+                        resaltarLineaCodigo(etiquetasCodigo, 2);
+                        Thread.sleep(TIEMPO_ESPERA);
+                        
+                        etiquetaArreglo.setText(arrayToString(arreglo));
+                        
+                        resaltarLineaCodigo(etiquetasCodigo, 3);
+                        Thread.sleep(TIEMPO_ESPERA);
+                        
+                        etiquetaArreglo.setText(arrayToString(arreglo));
+                        
+                        resaltarLineaCodigo(etiquetasCodigo, 4);
+                        Thread.sleep(TIEMPO_ESPERA);
+                    }
+                    
+                    arreglo[j] = valorActual;
+                    etiquetaArreglo.setText(arrayToString(arreglo));
+                }
+                return null;
+            }
+        };
+        
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
+        root.setPrefSize(400,400);
+        Anchor.getChildren().add(root);
+        return Anchor; 
     }
-    public static void main(String[] args) {
-        launch();
+     private int[] generarArregloAleatorio(int n, int min, int max) {
+        Random random = new Random();
+        int[] arreglo = new int[n];
+        for (int i = 0; i < n; i++) {
+            arreglo[i] = random.nextInt(max - min + 1) + min;
+        }
+        return arreglo;
     }
+    private void resaltarLineaCodigo(Text[] etiquetasCodigo, int indiceLinea) {
+        for (int i = 0; i < etiquetasCodigo.length; i++) {
+            if (i == indiceLinea) {
+                etiquetasCodigo[i].setFill(Color.RED);
+} else {
+etiquetasCodigo[i].setFill(Color.BLACK);
+}
+}
+}
+    private String arrayToString(int[] arreglo) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("[ ");
+    for (int i = 0; i < arreglo.length; i++) {
+        sb.append(arreglo[i]);
+        if (i < arreglo.length - 1) {
+            sb.append(", ");
+        }
+    }
+    sb.append(" ]");
+    return sb.toString();
+}
 
+public static void main(String[] args) {
+    launch(args);
+}
+    
+    
+    
+    }
+    
+  
     
     
 }
